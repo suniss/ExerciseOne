76024b89afcdc1310009788587828923d0bcceff
sunisakc <sunisa.kan@gmail.com>
2021-06-15 11:49:43 +0200

    Add project files.

---
+++ ExerciseOne/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExerciseOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
