05fa1324f6b181bdc278bcf597784bfda0d215c7
sunisakc <sunisa.kan@gmail.com>
2021-06-15 16:19:06 +0200

    User

--- ExerciseOne/Program.cs
+++ ExerciseOne/Program.cs
@@ -2,11 +2,33 @@
 
 namespace ExerciseOne
 {
+   
+    
     class Program
     {
+        public static string Name;
+        public static string LastName;
+        public static string Salary;
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to personal registation");
+            Console.WriteLine("Add personal name");
+            Name = Console.ReadLine();
+
+            Console.WriteLine("Add personal LastName");
+            LastName = Console.ReadLine();
+
+            Console.WriteLine("Add personal Salary");
+            Salary = Console.ReadLine();
+
+            createPersonal(Name, LastName, Salary);
+        }
+
+        public static void createPersonal(string Name, string LastName, string Salary)
+        {
+            Console.WriteLine("hi" + Name, LastName,Salary);
+            Console.WriteLine("Personal registered");
         }
     }
 }
